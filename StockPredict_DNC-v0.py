c0363c108e16c6bd7830c45fea6290da0f2a4f6d
KKuette <awakeproduction@hotmail.fr>
2017-05-27 19:24:55 +0200

    DNC not builded yet

--- StockPredict_DNC-v0.py
+++ StockPredict_DNC-v0.py
@@ -13,27 +13,28 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
 from keras.layers.recurrent import LSTM
 
+                                
+#Import csv file + traitement     
+                                
 
 def get_stock_data(stock_name, normalized=0):
-    location = './Datasets/Stock_price/CAC40.csv'
+    location = './Datasets/Stock_price/googl.csv'
 
-    col_names = ['Date','Open','High','Low','Close','Volume','Adj Close']
+    col_names = ['Date','Open','High','Low','Close','Volume']
     stocks = pd.read_csv(location, header=0, names=col_names)
     df = pd.DataFrame(stocks)
     date_split = df['Date'].str.split('-').str
     df['Year'], df['Month'], df['Day'] = date_split
     df["Volume"] = df["Volume"] / 10000
-    df.drop(df.columns[[0,3,5,6, 7,8,9]], axis=1, inplace=True)
+    df.drop(df.columns[[0,3,5,6, 7,8]], axis=1, inplace=True)
     return df
 
-stock_name = 'CAC40'
+stock_name = 'GOOGL'
 df = get_stock_data(stock_name,0)
 df.head()
 
-today = datetime.date.today()
-file_name = stock_name+'_stock_%s.csv' % today
+file_name = stock_name+'_stock_%s.csv'
 df.to_csv(file_name)
-
 df['High'] = df['High'] / 100
 df['Open'] = df['Open'] / 100
 df['Close'] = df['Close'] / 100
@@ -60,28 +61,32 @@ def load_data(stock, seq_len):
 
     return [x_train, y_train, x_test, y_test]
 
-def build_model(layers):
-    model = Sequential()
+    ###
 
-    model.add(LSTM(
-                 input_dim=layers[0],
-                 output_dim=layers[1],
-                  return_sequences=True))
-    model.add(Dropout(0.2))
+#def build_model(layers):
+    #model = Sequential()
 
-    model.add(LSTM(
-                layers[2],
-                return_sequences=False))
-    model.add(Dropout(0.2))
+    #model.add(LSTM(
+    #             input_dim=layers[0],
+    #             output_dim=layers[1],
+    #              return_sequences=True))
+    #model.add(Dropout(0.2))
 
-    model.add(Dense(
-                output_dim=layers[2]))
-    model.add(Activation("linear"))
+    #model.add(LSTM(
+    #            layers[2],
+    #            return_sequences=False))
+    #model.add(Dropout(0.2))
 
-    start = time.time()
-    model.compile(loss="mse", optimizer="rmsprop",metrics=['accuracy'])
-    print("Compilation Time : ", time.time() - start)
-    return model
+    #model.add(Dense(
+    #            output_dim=layers[2]))
+    #model.add(Activation("linear"))
+
+    #start = time.time()
+    #model.compile(loss="mse", optimizer="rmsprop",metrics=['accuracy'])
+    #print("Compilation Time : ", time.time() - start)
+    #return modeli
+
+    ###
 
 def build_model2(layers):
     d = 0.2
@@ -90,8 +95,8 @@ def build_model2(layers):
     model.add(Dropout(d))
     model.add(LSTM(64, input_shape=(layers[1], layers[0]), return_sequences=False))
     model.add(Dropout(d))
-    model.add(Dense(16,init='uniform',activation='relu'))        
-    model.add(Dense(1,init='uniform',activation='linear'))
+    model.add(Dense(16, activation='relu', kernel_initializer="uniform"))
+    model.add(Dense(1,  activation='linear', kernel_initializer="uniform"))
     model.compile(loss='mse',optimizer='adam',metrics=['accuracy'])
     return model
 
@@ -103,14 +108,22 @@ print("y_train", y_train.shape)
 print("X_test", X_test.shape)
 print("y_test", y_test.shape)
 
+                
+#Training       
+                
+
 model = build_model2([3,window,1])
 
 model.fit(
-            X_train,
-            y_train,
-            batch_size=512,
-            nb_epoch=500,
-            validation_split=0.1,
-            verbose=1)
+        X_train,
+        y_train,
+        batch_size=512,
+        epochs=500,
+        validation_split=0.1,
+        verbose=1)
 
+trainScore = model.evaluate(X_train, y_train, verbose=0)
+print('Train Score: %.2f MSE (%.2f RMSE)' % (trainScore[0], math.sqrt(trainScore[0])))
 
+testScore = model.evaluate(X_test, y_test, verbose=0)
+print('Test Score: %.2f MSE (%.2f RMSE)' % (testScore[0], math.sqrt(testScore[0])))
