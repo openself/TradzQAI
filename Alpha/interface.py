1920321eff233d2d5235575d8195657f2807caf0
kkuette <awakeproduction@hotmail.fr>
2017-11-23 08:49:14 +0100

    Added GUI + env

--- Alpha/interface.py
+++ Alpha/interface.py
@@ -1,8 +1,14 @@
 from tkinter import *
 import tkinter.ttk as ttk
-import RNN
+import train
+from threading import Thread
 from tkinter.filedialog import *
 from tkinter.messagebox import *
+from environnement import *
+import pandas as pd
+
+env = environnement()
+env.stock_name = "DAX30_2011_01"
 
 class interface(Frame):
 
@@ -10,23 +16,116 @@ class interface(Frame):
         self.name = "TradzQAI"
         self.version = "Alpha v0.1"
         window.title(self.name + " " + self.version)
+        window.resizable(0, 0)
+        window.geometry("1024x720")
+        self.m = my_thread()
+        self.m.daemon=True
         Frame.__init__(self, window, **kwargs)
         self.pack(fill=BOTH)
         self.text = Text(window, width=100, height=100)
         self.text.insert(INSERT, self.name + " " + self.version)
-        self.text.pack()
-        self.parsing = Button(self, text="Start parsing", command=quit)
-        self.parsing.pack()
-        self.run_ai = Button(self, text="Run AI", command=RNN.full_run)
-        self.run_ai.pack()
+
+        self.l = LabelFrame(window, text="Agent values", padx=2, pady=2)
+        self.l.pack(fill="both", expand="no", side=LEFT)
+
+        self.inventory = LabelFrame(window, text="Agent inventory", padx=2, pady=2)
+        self.inventory.pack(fill="both", expand="no", side=RIGHT)
+
+        self.order = LabelFrame(window, text="Agent trade", padx=2, pady=2)
+        self.order.pack(fill="both", expand="no")
+
+        self.ordr = pd.DataFrame(columns=['Orders'])
+        self.init_label()
+        '''
+        self.w = PanedWindow(window, orient=HORIZONTAL)
+        self.w.pack(side=TOP, pady=2, padx=2)
+        self.w.pack()
+        '''
+        self.parsing = Button(self, text="Pause", command=self.m.p)
+        self.parsing.pack(side=RIGHT, fill=BOTH)
+        self.but = Button(self, text="Resume", command=self.m.r)
+        self.but.pack(fill=BOTH, side=RIGHT)
+        self.run_ai = Button(self, text="Run", command=self.go_run)
+        self.run_ai.pack(side=RIGHT, fill=BOTH)
+
+    def manage_orders(self):
+        if len(self.ordr['Orders']) > 30:
+            self.ordr = (self.ordr.drop(0)).reset_index(drop=True)
+        if env.POS_BUY > -1 or env.POS_SELL > -1:
+            if len(env.inventory['Price']) > 0:
+                new = ["Close : " + str(env.cdata) + " -> " + str(env.corder) + " : " + str((env.inventory['Price']).iloc[0])]
+                tmp = pd.DataFrame(new, columns = ['Orders'])
+                self.ordr = self.ordr.append(tmp, ignore_index=True)
+
+    def init_ltxt(self):
+        self.manage_orders()
+
+        self.p = StringVar()
+        self.p.set("Profit : " + str(env.profit))
+
+        self.tp = StringVar()
+        self.tp.set("Total profit : " + str(env.total_profit))
+
+        self.r = StringVar()
+        self.r.set("Current reward : " + str(env.reward))
+
+        self.o = StringVar()
+        self.o.set("Order type : " + str(env.corder))
+
+        self.inv = StringVar()
+        self.inv.set(str(env.inventory))
+
+        self.ord = StringVar()
+        self.ord.set("None")
+
+    def init_label(self):
+        self.init_ltxt()
+        Label(self.l, textvariable=self.o).pack()
+        Label(self.l, textvariable=self.r).pack()
+        Label(self.l, textvariable=self.p).pack()
+        Label(self.l, textvariable=self.tp).pack()
+        Label(self.inventory, textvariable=self.inv).pack()
+        Label(self.order, textvariable=self.ord).pack()
+        
+
+    def go_run(self):
+        self.m.start()
+        #self.update()
 
     def text_update(self, state):
         self.text.insert(END, "\n")
         self.text.insert(END, state)
         self.pack()
 
+    def update(self):
+        self.manage_orders()
+        self.p.set("Profit : " + str(env.profit))
+        self.tp.set("Total profit : " + str(env.total_profit))
+        self.r.set("Current reward : " + str(env.reward))
+        self.o.set("Order type : " + str(env.corder))
+        self.inv.set(str(env.inventory))
+        self.ord.set(str(self.ordr))
+        self.l.update_idletasks()
+
+class my_thread(Thread):
+
+    def __init__(self):
+        Thread.__init__(self)
+
+    def p(self):
+        self.t.b = 1
+
+    def r(self):
+        self.t.b = 0
+
+    def run(self):
+        self.t = train.train(env, interfaces)
+        self.t.training()
+
+
 def launch_interface():
-    window = Tk()
-    interfaces = interface(window)
     interfaces.mainloop()
-    return interfaces
+
+
+window = Tk()
+interfaces = interface(window)
--- Alpha/train.py
+++ Alpha/train.py
@@ -1,29 +1,53 @@
 from agent import Agent
 from functions import *
 from math import *
+import time
 import pandas as pd
 import sys
+from environnement import *
+from interface import *
 
+class train(environnement):
 
-class train():
-
-    def __init__(self, stock_name, window_size, episode_count):
-        self.agent = Agent(window_size)
-        self.stock_name = stock_name
-        self.data = getStockDataVec(stock_name)
+    def __init__(self, env, interface):
+        self.env = env
+        self.interface = interface
+        self.window_size = env.window_size
+        self.agent = Agent(self.window_size)
+        self.stock_name = env.stock_name
+        self.data = getStockDataVec(self.stock_name)
         self.l = len(self.data) - 1
         self.batch_size = 32
-        self.contract_price = 5
+        self.contract_price = env.contract_price
         self.columns = ['Price', 'POS']
-        self.max_order = 5
-        self.spread = 1
-        self.episode_count = episode_count
-        self.window_size = window_size
-        self.total_profit = 0
-        self.reward = 0
-        self.profit = 0
-        self.buy_price = 0
-        self.sell_price = 0
+        self.max_order = env.max_order
+        self.spread = env.spread
+        self.episode_count = env.episode_count
+        self.total_profit = env.total_profit
+        self.reward = env.reward
+        self.profit = env.profit
+        self.buy_price = env.buy_price
+        self.sell_price = env.sell_price
+        self.b = 0
+        self.corder = ""
+        self.cdata = 0
+
+    def update_env(self, env):
+        env.POS_SELL = self.env.POS_SELL
+        env.POS_BUY = self.env.POS_BUY
+        env.data = self.data
+        env.contract_price = self.contract_price
+        env.max_order = self.max_order
+        env.spread = self.spread
+        env.total_profit = self.total_profit
+        env.reward = self.reward
+        env.profit = self.profit
+        env.buy_price = self.buy_price
+        env.sell_price = self.sell_price
+        env.pause = self.b
+        env.corder = self.corder
+        env.inventory = self.agent.inventory
+        env.cdata = self.cdata
 
     def src_sell(self, inventory):
         for i  in range(len(inventory)):
@@ -40,11 +64,22 @@ class train():
     def training(self):
         for e in range(self.episode_count + 1):
 
+            if self.b == 1:
+                while (self.b==1):
+                    time.sleep(self.b)
+
             print ("Episode " + str(e) + "/" + str(self.episode_count))
             state = getState(self.data, 0, self.window_size + 1)
             self.agent.inventory = pd.DataFrame(columns = self.columns)
 
             for t in range(self.l):
+                self.cdata = self.data[t]
+                self.env.POS_SELL = -1
+                self.env.POS_BUY = -1
+
+                if self.b == 1:
+                    while (self.b==1):
+                        time.sleep(self.b)
 
                 POS = len(self.agent.inventory['POS'])
                 action = self.agent.act(state)
@@ -55,7 +90,9 @@ class train():
                 self.profit = 0
 
                 if action == 1: # buy
+                    self.corder = "BUY"
                     POS_SELL = self.src_sell(self.agent.inventory['POS'])
+                    self.env.POS_SELL = POS_SELL
                     if POS_SELL == -1:
                         buy = (pd.DataFrame([self.buy_price], columns = [self.columns[0]])).join(pd.DataFrame(["BUY"], columns = [self.columns[1]]))
                         self.agent.inventory = self.agent.inventory.append(buy, ignore_index=True)
@@ -63,14 +100,16 @@ class train():
                         self.profit = (self.agent.inventory['Price'][POS_SELL] - self.sell_price) * self.contract_price
                         self.total_profit += self.profit
                         self.agent.inventory = (self.agent.inventory.drop(self.agent.inventory.index[POS_SELL])).reset_index(drop=True)
+                    if self.profit > 0:
+                        self.reward = self.profit
                     if POS > self.max_order:
                         self.reward = -(POS)
-                    if self.profit > 0:
-                        self.reward += self.profit
-                    print ("Buy: " + formatPrice(self.buy_price) + "| Profit: " + formatPrice(self.profit), "| total:", formatPrice(self.total_profit)," Data :", t, "/", self.l)
+                   # print ("Buy: " + formatPrice(self.buy_price) + "| Profit: " + formatPrice(self.profit), "| total:", formatPrice(self.total_profit)," Data :", t, "/", self.l)
 
                 elif action == 2: # sell
+                    self.corder = "SELL"
                     POS_BUY = self.src_buy(self.agent.inventory['POS'])
+                    self.env.POS_BUY = POS_BUY
                     if POS_BUY == -1:
                         sell = (pd.DataFrame([self.sell_price], columns = [self.columns[0]])).join(pd.DataFrame(["SELL"], columns = [self.columns[1]]))
                         self.agent.inventory = self.agent.inventory.append(sell, ignore_index=True)
@@ -78,11 +117,11 @@ class train():
                         self.profit = (self.buy_price - self.agent.inventory['Price'][POS_BUY]) * self.contract_price
                         self.total_profit += self.profit
                         self.agent.inventory = (self.agent.inventory.drop(self.agent.inventory.index[POS_BUY])).reset_index(drop=True)
+                    if self.profit > 0:
+                        self.reward = self.profit
                     if POS > self.max_order:
                         self.reward = -(POS)
-                    if self.profit > 0:
-                        self.reward += self.profit
-                    print ("Sell: " + formatPrice(self.sell_price) + "| Profit: " + formatPrice(self.profit), "| total:", formatPrice(self.total_profit)," Data :", t, "/", self.l)
+                    #print ("Sell: " + formatPrice(self.sell_price) + "| Profit: " + formatPrice(self.profit), "| total:", formatPrice(self.total_profit)," Data :", t, "/", self.l)
                 else:
                     if POS > self.max_order:
                         self.reward = -POS
@@ -93,15 +132,19 @@ class train():
                 state = next_state
                 if len(self.agent.memory) > self.batch_size:
                     self.agent.expReplay(self.batch_size)
+                self.update_env(self.env)
+                self.interface.update()
+
             if e % 10 == 0:
                 self.agent.model.save("models/model_" + str(self.stock_name) + "_ws_" + str(self.window_size) +"_ep_" + str(e))
+
         
 
-
+'''
 if len(sys.argv) != 4:
 	print ("Usage: python train.py [stock] [window] [episodes]")
 	exit()
 
 
 t = train(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
-t.training()
+'''
