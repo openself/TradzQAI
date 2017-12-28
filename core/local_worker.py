30b940791b8e41f182c16fb28f0aa82bd09fca00
kkuette <awakeproduction@hotmail.fr>
2017-12-28 12:58:37 +0100

    moved run fct in local_worker

--- core/local_worker.py
+++ core/local_worker.py
@@ -1,8 +1,107 @@
 from PyQt5.QtCore import *
 
-class Local_Worker(QThread):
+from .worker import Worker
+from environnement import Environnement
+from tools import *
 
-    sig_step = pyqtSignal()
+import time
 
-    def __init__(self):
-        QThread.__init__(self)
+import pandas as pd
+
+class Local_Worker(Worker):
+
+    def __init__(self, env):
+        self.env = env
+        Worker.__init__(self, env)
+
+    def run(self):
+        self.init_agent()
+
+        for e in range(self.env.episode_count + 1):
+            self.env.cepisode = e + 1
+            self.env.start_t = time.time()
+            self.agent.inventory = pd.DataFrame(columns = self.columns) # Init agent inventory
+
+            if self.env.pause == 1:
+                while (self.env.pause == 1):
+                    time.sleep(0.01)
+
+            state = getState(self.raw,
+                             0,
+                             self.env.window_size + 1)
+
+            for t in range(self.env.data):
+                if self.env.pause == 1:
+                    while (self.env.pause == 1):
+                        time.sleep(0.01)
+                b = 0
+                tmp = time.time()
+                self.env.cdatai = t
+                self.env.cdata = self.data[t]
+                self.env.lst_data.append(self.env.cdata)
+                self.env.POS_SELL = -1
+                self.env.POS_BUY = -1
+
+                done = True if t == self.env.data - 1 else False
+
+                self.action = self.agent.act(state) # Get action from agent
+                self.env.def_act(self.action)
+
+                # Get new state
+                next_state = getState(self.raw,
+                                      t + 1,
+                                      self.env.window_size + 1)
+                self.env.lst_state.append(self.env.cdata)
+
+                # Update buy price with spread
+                self.env.buy_price = self.data[t] - (self.env.spread / 2)
+                # Update sell price with spread
+                self.env.sell_price = self.data[t] + (self.env.spread / 2)
+
+                self.env.reward = 0 # Reset reward
+                self.env.profit = 0 # Reset current profit
+
+                self.env.manage_exposure()
+
+                if self.pip_drawdown_checking() == 0:
+                    self.inventory_managment()
+
+                self.update_env() # Updating env from agent for GUI
+                self.env.manage_wallet()
+
+                if self.env.manage_date() == 1:
+                    self.env.reward += self.env.daily_win - self.env.daily_loose
+                    self.env.daily_win = 0
+                    self.env.daily_loose = 0
+
+                if self.env.cmax_pos < 1 or self.env.cmax_pos <= int(self.env.max_pos / 2):
+                    self.env.capital = self.env.scapital
+                    b = 1
+                    self.env.reward -= 1000
+                    done = True
+
+                self.sig_step.emit() # Update GUI
+                self.agent.memory.append((state,
+                                          self.action,
+                                          self.env.reward,
+                                          next_state,
+                                          done))
+                state = next_state
+
+                if "train" in self.env.mode:
+                    if len(self.agent.memory) > self.env.batch_size:
+                        self.sig_batch.emit()
+                        self.env.lst_loss.append(self.agent.expReplay(self.env.batch_size))
+                    if t % 1000 == 0 and t > 0 : # Save model all 1000 data
+                        self.agent._save_model()
+                        if "DDQN" == self.env.model_name or "DDRQN" == self.env.model_name or "DDPG" == self.env.model_name:
+                            self.agent.update_target_model()
+                else:
+                    time.sleep(0.01)
+                self.env.loop_t = time.time() - tmp
+
+                if b == 1:
+                    break
+
+            self.env.manage_h_lst()
+            self.sig_episode.emit()
--- core/worker.py
+++ core/worker.py
@@ -4,8 +4,6 @@ import time
 import pandas as pd
 import sys
 
-from mem_top import mem_top
-
 from environnement import Environnement
 from tools import *
 
@@ -14,6 +12,8 @@ from PyQt5.QtCore import *
 class Worker(QThread):
 
     sig_step = pyqtSignal()
+    sig_batch = pyqtSignal()
+    sig_episode = pyqtSignal()
 
     def __init__(self, env):
         QThread.__init__(self)
@@ -25,8 +25,6 @@ class Worker(QThread):
         self.series = 0
 
     def init_agent(self):
-        self.data, self.raw = getStockDataVec(self.env.stock_name)
-        self.l = len(self.data) - 1
         if "eval" in self.env.mode:
             is_eval = True
         else:
@@ -43,10 +41,19 @@ class Worker(QThread):
 
         elif "DDRQN" == self.env.model_name:
             from agents import DDRQN as Agent
-        
+
         elif "DDPG" == self.env.model_name:
             from agents import DDPG as Agent
 
+        elif "EIIE" == self.env.model_name:
+            pass
+
+        else:
+            raise ValueError('could not import %s' % self.env.model_name)
+
+        self.data, self.raw, self.env.date = getStockDataVec(self.env.stock_name)
+        self.env.check_dates()
+        self.env.data = len(self.data) - 1
         self.agent = Agent(getState(self.raw,
                                     0,
                                     self.env.window_size + 1),
@@ -60,7 +67,6 @@ class Worker(QThread):
         self.agent.build_model()
 
     def update_env(self):
-        self.env.data = self.l
         self.env.inventory = self.agent.inventory
         self.env.lst_reward.append(self.env.tot_reward)
         self.env.lst_return.append(self.env.total_profit)
@@ -90,7 +96,8 @@ class Worker(QThread):
 
     def inventory_managment(self):
         POS = len(self.agent.inventory['POS']) # Number of contract in inventory
-        if "BUY" in self.env.corder:
+        if 1 == self.action: # Buy
+            self.env.corder = "BUY"
             POS_SELL = self.src_sell(self.agent.inventory['POS']) # Check if SELL order in inventory
             self.env.POS_SELL = POS_SELL # Put check in env
 
@@ -104,14 +111,16 @@ class Worker(QThread):
                 Calc profit and total profit
                 Add last Sell order to env
                 '''
-                POS_SELL = self.src_best_buy(self.agent.inventory['Price'])
+                #POS_SELL = self.src_best_buy(self.agent.inventory['Price'])
                 self.env.profit = self.agent.inventory['Price'][POS_SELL] - self.env.sell_price
                 if self.env.profit < 0.00:
                     self.env.loose += 1
+                    self.env.daily_loose += 1
                     self.env.reward += 0.5 * self.env.profit
                     #self.series = 0
                 elif self.env.profit > 0.00 :
                     self.env.win += 1
+                    self.env.daily_win += 1
                     self.env.reward += self.env.profit #+ int(sqrt(self.series))
                     #self.series += 1
                 else:
@@ -122,7 +131,8 @@ class Worker(QThread):
             else:
                 self.env.reward -= 0.5
 
-        elif "SELL" in self.env.corder:
+        elif 2 == self.action: # Sell
+            self.env.corder = "SELL"
             POS_BUY = self.src_buy(self.agent.inventory['POS']) # Check if BUY order in inventory
             self.env.POS_BUY = POS_BUY # Put check in env
 
@@ -136,14 +146,16 @@ class Worker(QThread):
                 Calc profit and total profit
                 Add last Sell order to env
                 '''
-                POS_BUY = self.src_best_sell(self.agent.inventory['Price'])
+                #POS_BUY = self.src_best_sell(self.agent.inventory['Price'])
                 self.env.profit = self.env.buy_price - self.agent.inventory['Price'][POS_BUY]
                 if self.env.profit < 0:
                     self.env.loose += 1
+                    self.env.daily_loose += 1
                     self.env.reward += 0.5 * self.env.profit
                     #self.series = 0
                 elif self.env.profit > 0 :
                     self.env.win += 1
+                    self.env.daily_win += 1
                     self.env.reward += self.env.profit #+ int(sqrt(self.series))
                     #self.series += 1
                 else:
@@ -153,6 +165,8 @@ class Worker(QThread):
                 self.save_last_closing(POS_BUY)
             else:
                 self.env.reward -= 0.5
+        else: # Hold
+            self.env.reward -= 0.25
 
     def save_last_closing(self, POS):
         '''
@@ -191,103 +205,3 @@ class Worker(QThread):
             if "BUY" in inventory.loc[i]:
                 return (i)
         return (-1)
-
-    def run(self):
-        self.init_agent()
-        for e in range(self.env.episode_count + 1):
-            self.env.cepisode = e + 1
-            self.env.total_profit = 0
-            self.env.win = 0
-            self.env.loose = 0
-            self.env.draw = 0
-
-            self.env.start_t = time.time()
-            self.agent.inventory = pd.DataFrame(columns = self.columns) # Init agent inventory
-
-            if self.env.pause == 1:
-                while (self.env.pause == 1):
-                    time.sleep(0.01)
-
-            state = getState(self.raw,
-                             0,
-                             self.env.window_size + 1)
-
-            for t in range(self.l):
-                b = 0
-                tmp = time.time()
-                self.env.cdatai = t
-                self.env.cdata = self.data[t]
-                self.env.lst_data.append(self.env.cdata)
-                self.env.POS_SELL = -1
-                self.env.POS_BUY = -1
-
-                if self.env.pause == 1:
-                    while (self.env.pause == 1):
-                        time.sleep(0.01)
-                
-                done = True if t == self.l - 1 else False
-
-                POS = len(self.agent.inventory['POS'])
-
-                self.action = self.agent.act(state) # Get action from agent
-                # Get new state
-                next_state = getState(self.raw,
-                                      t + 1,
-                                      self.env.window_size + 1)
-                self.env.lst_state.append(self.env.cdata)
-
-                # Update buy price with spread
-                self.env.buy_price = self.data[t] - (self.env.spread / 2)
-                # Update sell price with spread
-                self.env.sell_price = self.data[t] + (self.env.spread / 2)
-
-                self.env.reward = 0 # Reset reward
-                self.env.profit = 0 # Reset current profit
-
-                self.env.manage_exposure()
-
-                if self.pip_drawdown_checking() == 0:
-                    if self.action == 1: # buy
-                        self.env.corder = "BUY"
-                        self.inventory_managment()
-
-                    elif self.action == 2: # sell
-                        self.env.corder = "SELL"
-                        self.inventory_managment()
-
-                    else:
-                        self.env.reward -= 0.25
-
-                self.env.def_act(self.action)
-                self.update_env() # Updating env from agent for GUI
-                self.env.manage_wallet()
-
-                if self.env.cmax_pos < 1 or self.env.cmax_pos <= int(self.env.max_pos / 2):
-                    self.env.capital = self.env.scapital
-                    b = 1
-                    self.env.reward = -1000
-                    done = True
-
-                self.sig_step.emit() # Update GUI
-
-
-                self.agent.memory.append((state,
-                                          self.action,
-                                          self.env.reward,
-                                          next_state,
-                                          done))
-                state = next_state
-
-                if "train" in self.env.mode:
-                    if len(self.agent.memory) > self.env.batch_size:
-                        self.env.lst_loss.append(self.agent.expReplay(self.env.batch_size))
-                    if t % 1000 == 0 and t > 0 : # Save model all 1000 data
-                        self.agent._save_model()
-                        if "DDQN" == self.env.model_name or "DDRQN" == self.env.model_name or "DDPG" == self.env.model_name:
-                            self.agent.update_target_model()
-                else:
-                    time.sleep(0.01)
-
-                self.env.loop_t = time.time() - tmp
-                if b == 1:
-                    break
