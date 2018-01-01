a608d43a98ed1866b8c746496125e57ab88a5ac3
kkuette <awakeproduction@hotmail.fr>
2018-01-01 16:08:27 +0100

    Added error in start window + Added QRadioButton to choose mode

--- GUI/main_window.py
+++ GUI/main_window.py
@@ -12,7 +12,7 @@ from PyQt5.QtCore import *
 env = Environnement()
 
 h = 950
-w = 400
+w = 500
 
 class MainWindow(QWidget):
 
@@ -33,74 +33,218 @@ class Start_Window(QWidget):
         super(QWidget, self).__init__(root)
         #env.ui = self
         self.root = root
+        self.error = False
+        self.Wtrain = None
+        self.Weval = None
         self.Build_Swindow()
 
     def Build_Swindow(self):
-        self.SFrame = QFrame(self)
-        self.SFrame.resize(h, w)
+        self.StFrame = QFrame(self)
 
         gbox = QGroupBox("Settings")
 
         HBlayout = QHBoxLayout()
-        HLayout = QHBoxLayout()
         VBlayout = QVBoxLayout()
         VBFrame = QFrame()
 
-        self.train = QPushButton('Train')
-        self.eval = QPushButton('Eval')
+        self.VFLayout = QVBoxLayout()
+
+        self.SFrame = QFrame()
+        SVbox = QVBoxLayout()
+
+        RBgbox = QGroupBox("Mode")
+        HRBlayout = QHBoxLayout()
+
+        self.rbtrain = QRadioButton("Train")
+        self.rbeval = QRadioButton("Eval")
+
+        self.rbtrain.toggled.connect(lambda: self.Show_f(self.rbtrain.text()))
+        self.rbeval.toggled.connect(lambda: self.Show_f(self.rbeval.text()))
+
+        HRBlayout.addWidget(self.rbtrain)
+        HRBlayout.addWidget(self.rbeval)
+
+        HRBlayout.setAlignment(Qt.AlignHCenter)
+        HRBlayout.setSpacing(350)
+
+        RBgbox.setLayout(HRBlayout)
+
+        self.next = QPushButton('Next')
         self.leave = QPushButton('Exit')
 
-        self.train.clicked.connect(self.Build_Train)
-        self.eval.clicked.connect(self.Build_Eval)
+        self.next.clicked.connect(self.check_error)
         self.leave.clicked.connect(quit)
 
-        HBlayout.addWidget(self.train)
-        HBlayout.addWidget(self.eval)
         HBlayout.addWidget(self.leave)
+        HBlayout.addWidget(self.next)
 
         VBFrame.setLayout(HBlayout)
+        gbox.setLayout(self.VFLayout)
+        SVbox.addWidget(gbox)
+        self.SFrame.setLayout(SVbox)
 
-        gbox_es = self._build_env_settings()
-        gbox_ws = self._build_wallet_settings()
-        gbox_ms = self._build_model_settings()
+        self.lerror = QLabel()
+
+        self.SFrame.hide()
+
+        VBlayout.addWidget(RBgbox)
+        VBlayout.addWidget(self.lerror)
+        VBlayout.addWidget(self.SFrame)
+        VBlayout.addWidget(VBFrame)
+        VBlayout.addStretch()
+
+        self.lerror.setAlignment(Qt.AlignHCenter)
+
+        self.StFrame.setLayout(VBlayout)
+
+        self.StFrame.resize(h, w)
+        self.root.resize(h, 150)
+
+    def _Build(self, mode):
+        Frame = QFrame()
+        Frame.setObjectName("Frame"+mode)
+        #Frame.hide()
+
+        HLayout = QHBoxLayout()
+
+        gbox_es = self._build_env_settings(mode)
+        gbox_ws = self._build_wallet_settings(mode)
+        gbox_ms = self._build_model_settings(mode)
 
         HLayout.addWidget(gbox_es)
         HLayout.addWidget(gbox_ws)
         HLayout.addWidget(gbox_ms)
 
-        gbox.setLayout(HLayout)
+        Frame.setLayout(HLayout)
 
-        VBlayout.addWidget(gbox)
-        VBlayout.addWidget(VBFrame)
+        return Frame
 
-        self.SFrame.setLayout(VBlayout)
+    def clearLayout(self, layout):
+        while layout.count():
+            child = layout.takeAt(0)
+            if child.widget() is not None:
+                child.widget().deleteLater()
+            else:
+                clearLayout(child.layout())
+
+    def Show_f(self, frame):
+        if frame == "Train":
+            self.StFrame.resize(h, w)
+            self.root.resize(h, w)
+            self.clearLayout(self.VFLayout)
+            self.Wtrain = self.BuildTrain()
+            self.VFLayout.addWidget(self.Wtrain)
+        elif frame == "Eval":
+            self.StFrame.resize(h, 425)
+            self.root.resize(h, 425)
+            self.clearLayout(self.VFLayout)
+            self.Weval = self.BuildEval()
+            self.VFLayout.addWidget(self.Weval)
+        self.SFrame.show()
+        self.error = False
+        self.color_error()
+        self.lerror.setText('')
+        self.lerror.setStyleSheet("QLabel {background-color : }")
+
+    def BuildTrain(self):
+        env.mode = "train"
+        return self._Build(env.mode)
+
+    def BuildEval(self):
+        env.mode = "eval"
+        return self._Build(env.mode)
+
+    def check_error(self):
+        if self.error is False:
+            self.Build_Primary_Window()
+
+    def color_error(self):
+        if self.error is True:
+            self.next.setStyleSheet("QPushButton {color: grey}")
+        else:
+            self.next.setStyleSheet("QPushButton {color: }")
 
     def check_changed_lr(self, value):
         try:
             ret = float(value)
             if ret >= 1 or ret == 0:
+                if ret >= 1:
+                    self.lerror.setText('Learning rate >= 1')
+                elif ret == 0:
+                    self.lerror.setText('Learning rate == 0')
                 self.lelr.setStyleSheet("QLineEdit {border-color : red}")
+                self.lerror.setStyleSheet("QLabel {background-color : red}")
+                self.error = True
             else:
-                # Glitched reset nvm :D
-                self.lelr.setStyleSheet("QLibeEdit {border-color : blue}")
+                self.lelr.setStyleSheet("QLineEdit {border-color : }")
+                self.lerror.setStyleSheet("QLabel {background-color : }")
+                self.lerror.setText('')
+                self.error = False
+            self.color_error()
         except:
             self.lelr.setStyleSheet("QLineEdit {border-color : red}")
+            self.lerror.setStyleSheet("QLabel {background-color : red}")
+            self.lerror.setText('Learning rate is not a float')
+            self.error = True
+            self.color_error()
 
     def check_changed_an(self, name):
         for agent in env.agents:
             if name == agent:
-                # Glitched reset nvm :D
-                self.lemn.setStyleSheet("QLibeEdit {border-color : red}")
+                self.lemn.setStyleSheet("QLineEdit {border-color : }")
+                self.lerror.setStyleSheet("QLabel {background-color : }")
+                self.lerror.setText('')
+                self.error = False
+                self.color_error()
                 return
         self.lemn.setStyleSheet("QLineEdit {border-color : red}")
+        self.lerror.setText('Can\'t find agent')
+        self.lerror.setStyleSheet("QLabel {background-color : red}")
+        self.error = True
+        self.color_error()
 
     def check_changed_dataset(self, data):
         if os.path.exists("data/"+data+".csv") == False:
             self.lem.setStyleSheet("QLineEdit {border-color : red}")
+            self.lerror.setText('Can\'t find dataset')
+            self.lerror.setStyleSheet("QLabel {background-color : red}")
+            self.error = True
         else:
-            self.lem.setStyleSheet("QLibeEdit {border-color : red}")
+            self.lem.setStyleSheet("QLineEdit {border-color : }")
+            self.lerror.setText('')
+            self.lerror.setStyleSheet("QLabel {background-color : }")
+            self.error = False
+        self.color_error()
 
-    def _build_wallet_settings(self):
+    def check_changed_exposure(self):
+        cap_exposure = self.sbc.value() - (self.sbc.value() * (1 - self.sbexposure.value() / 100))
+        max_order_valid = cap_exposure // (self.sbcp.value() + (self.sbmpd.value() * self.sbpv.value()))
+        if max_order_valid == 0:
+            self.change_exposure_varcolor('red')
+            self.lerror.setText('Can\'t take orders')
+            self.lerror.setStyleSheet("QLabel {background-color : red}")
+            self.error = True
+        elif max_order_valid < self.sbmo.value() // 2:
+            self.change_exposure_varcolor('red')
+            self.lerror.setText('Can\'t take '+str(int(self.sbmo.value() // 2))+' orders')
+            self.lerror.setStyleSheet("QLabel {background-color : red}")
+            self.error = True
+        else:
+            self.change_exposure_varcolor('')
+            self.lerror.setText('')
+            self.lerror.setStyleSheet("QLabel {background-color : }")
+            self.error = False
+        self.color_error()
+
+    def change_exposure_varcolor(self, color):
+        self.sbc.setStyleSheet("QSpinBox {border-color : " +color+ "}")
+        self.sbexposure.setStyleSheet("QDoubleSpinBox {border-color :" +color +"}")
+        self.sbmpd.setStyleSheet("QSpinBox {border-color : " +color+ "}")
+        self.sbmo.setStyleSheet("QSpinBox {border-color : "+color+"}")
+        self.sbcp.setStyleSheet("QSpinBox {border-color : "+color+"}")
+        self.sbpv.setStyleSheet("QSpinBox {border-color : "+color+"}")
+
+    def _build_wallet_settings(self, mode):
         Glayout = QGridLayout()
         gbox = QGroupBox("Wallet and risk settings")
 
@@ -138,15 +282,20 @@ class Start_Window(QWidget):
         Glayout.addWidget(lmo, 3, 0)
         Glayout.addWidget(self.sbmo, 3, 1)
 
+        self.sbc.valueChanged.connect(self.check_changed_exposure)
+        self.sbexposure.valueChanged.connect(self.check_changed_exposure)
+        self.sbmpd.valueChanged.connect(self.check_changed_exposure)
+        self.sbmo.valueChanged.connect(self.check_changed_exposure)
+
         gbox.setLayout(Glayout)
 
         return gbox
 
-    def _build_model_settings(self):
+    def _build_model_settings(self, mode):
         Glayout = QGridLayout()
         gbox = QGroupBox("Model settings")
 
-        lmn = QLabel('Name : ')
+        lmn = QLabel('Agent : ')
         self.lemn = QLineEdit()
         self.lemn.setText(str(env.model_name))
 
@@ -170,12 +319,13 @@ class Start_Window(QWidget):
 
         Glayout.addWidget(lmn, 0, 0)
         Glayout.addWidget(self.lemn, 0, 1)
-        Glayout.addWidget(llr, 1, 0)
-        Glayout.addWidget(self.lelr, 1, 1)
-        Glayout.addWidget(lg, 2, 0)
-        Glayout.addWidget(self.leg, 2, 1)
-        Glayout.addWidget(le, 3, 0)
-        Glayout.addWidget(self.lee, 3, 1)
+        if mode == "train":
+            Glayout.addWidget(llr, 1, 0)
+            Glayout.addWidget(self.lelr, 1, 1)
+            Glayout.addWidget(lg, 2, 0)
+            Glayout.addWidget(self.leg, 2, 1)
+            Glayout.addWidget(le, 3, 0)
+            Glayout.addWidget(self.lee, 3, 1)
 
         self.lelr.textChanged.connect(self.check_changed_lr)
         self.lemn.textChanged.connect(self.check_changed_an)
@@ -184,7 +334,7 @@ class Start_Window(QWidget):
 
         return gbox
 
-    def _build_env_settings(self):
+    def _build_env_settings(self, mode):
         Glayout = QGridLayout()
         gbox = QGroupBox("Environnement settings")
 
@@ -237,14 +387,17 @@ class Start_Window(QWidget):
         Glayout.addWidget(self.sbpv, 2, 1)
         Glayout.addWidget(ls, 3, 0)
         Glayout.addWidget(self.sbs, 3, 1)
-        Glayout.addWidget(lec, 4, 0)
-        Glayout.addWidget(self.sbec, 4, 1)
-        Glayout.addWidget(lws, 5, 0)
-        Glayout.addWidget(self.sbws, 5, 1)
-        Glayout.addWidget(lbs, 6, 0)
-        Glayout.addWidget(self.sbbs, 6, 1)
+        if mode == "train":
+            Glayout.addWidget(lec, 4, 0)
+            Glayout.addWidget(self.sbec, 4, 1)
+            Glayout.addWidget(lws, 5, 0)
+            Glayout.addWidget(self.sbws, 5, 1)
+            Glayout.addWidget(lbs, 6, 0)
+            Glayout.addWidget(self.sbbs, 6, 1)
 
         self.lem.textChanged.connect(self.check_changed_dataset)
+        self.sbcp.valueChanged.connect(self.check_changed_exposure)
+        self.sbpv.valueChanged.connect(self.check_changed_exposure)
 
         gbox.setLayout(Glayout)
 
@@ -258,22 +411,35 @@ class Start_Window(QWidget):
         self.root.showMaximized()
 
     def Hide_Swindow(self):
-        self.SFrame.hide()
+        self.StFrame.hide()
 
     def Show_Swindow(self):
-        self.SFrame.show()
+        self.StFrame.show()
 
-    def Build_Eval(self):
-        env.mode = "eval"
-        self.Build_Primary_Window()
+    def _get_env_var(self):
+        env.stock_name = self.lem.text()
+        env.model_name = self.lemn.text()
+        env.learning_rate = float(self.lelr.text())
 
-    def Build_Train(self):
-        env.mode = "train"
-        self.Build_Primary_Window()
+        env.gamma = self.leg.value()
+        env.epsilon = self.lee.value()
+        env.episode_count = self.sbec.value()
+        env.window_size = self.sbws.value()
+        env.sbbs = self.sbbs.value()
+        env.capital = self.sbc.value()
+        env.exposure = self.sbexposure.value()
+        env.max_pip_drawdown = self.sbmpd.value()
+        env.max_pos = self.sbmo.value()
+        env.pip_value = self.sbpv.value()
+        env.spread = self.sbs.value()
+        env.contract_price = self.sbcp.value()
 
     def Build_Primary_Window(self):
         self.Hide_Swindow()
         self._resize()
+
+        if env.mode == "Eval":
+            env.episode_count = 1
 
         self.worker = Worker(env)
         self.worker.sig_step.connect(self.update)
@@ -281,11 +447,7 @@ class Start_Window(QWidget):
         self.worker.sig_episode.connect(self.episode_up)
 
         # Getting env settings
-        env.contract_price = self.sbcp.value()
-        env.stock_name = self.lem.text()
-        env.spread = self.sbs.value()
-        env.max_pos = self.sbmo.value()
-        env.pip_value = self.sbpv.value()
+        self._get_env_var()
 
         VLayout = QVBoxLayout(self)
         HLayout = QHBoxLayout()
